3ccfee394598f227c5bcbdbab2436fd964ededba
Vitor Canineo Komar <vitorcanineokomar@hotmail.com.br>
2025-10-26 10:54:42 -0300

    added support for multiple input words

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("word")
+    parser.add_argument("words", nargs="+")
     parser.add_argument("-f", "--from-lang", default="es")
     parser.add_argument("-t", "--to-lang", default="en")
     parser.add_argument("-n", "--number-of-words", default=3)
@@ -17,34 +17,39 @@ def main():
     parser.add_argument("-d", "--deck", default="test")
     args = parser.parse_args()
 
-    pairs = get_sentences_with_word(args.word, args.from_lang, args.to_lang, args.number_of_words)
-    translation = get_word_translation(args.word, args.from_lang, args.to_lang)
-
-    word_with_article = args.word
-    if args.from_lang == "de":
-        article = get_german_word_article(args.word)
-        if article:
-            word_with_article = article + " " + word_with_article
-
-    generate_sentence_audio(word_with_article, args.from_lang)
-    for pair in pairs:
-        sentence = pair[0]
-        generate_sentence_audio(sentence, args.from_lang)
-
-    my_note = generate_card(word_with_article, translation, pairs, audio=True)
-    reversed_note = generate_reverse_card(args.word, translation, pairs, audio=True)
-
-
     my_deck = genanki.Deck(
         2059400110,
         args.deck)
+    
+    media_files = []
+    for word in args.words:
+        pairs = get_sentences_with_word(word, args.from_lang, args.to_lang, args.number_of_words)
+        translation = get_word_translation(word, args.from_lang, args.to_lang)
 
-    my_deck.add_note(my_note)
-    my_deck.add_note(reversed_note)
+        word_with_article = word
+        if args.from_lang == "de":
+            article = get_german_word_article(word)
+            if article:
+                word_with_article = article + " " + word_with_article
+
+        generate_sentence_audio(word_with_article, args.from_lang)
+        for pair in pairs:
+            sentence = pair[0]
+            generate_sentence_audio(sentence, args.from_lang)
+
+        my_note = generate_card(word_with_article, translation, pairs, audio=True)
+        reversed_note = generate_reverse_card(word, translation, pairs, audio=True)
+
+
+        media_files += [word_with_article + ".mp3" ]+ [
+            pair[0]+".mp3" for pair in pairs
+        ]
+
+        my_deck.add_note(my_note)
+        my_deck.add_note(reversed_note)
+
     pkg = genanki.Package(my_deck)
-    pkg.media_files = [word_with_article + ".mp3" ]+ [
-        pair[0]+".mp3" for pair in pairs
-    ]
+    pkg.media_files = media_files
     pkg.write_to_file(args.deck+".apkg")
 
     for p in Path(".").glob("*.mp3"):
